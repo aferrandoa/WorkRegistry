b381bbbb2a033260719f04819d5659c19337b9a3
Fernando Abalos <alfd@gft.com>
2016-10-03 16:32:43 +0200

    Added karma

--- WebContent/app/baseTemplate/header.componen.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-describe('MyList Tests', function () {
-    it('Should get 5 dogs', function () {
-        var list = ['perro'];
-        expect(list.length).toBe(1);
-    });
-});
-//# sourceMappingURL=header.componen.spec.js.map
